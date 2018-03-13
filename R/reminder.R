e84718e769fb79298f4994f8741e938867c81d2b
David Zumbach <31518032+zumbov2@users.noreply.github.com>
2018-03-13 11:27:37 +0100

    0.2.1

--- R/reminder.R
+++ R/reminder.R
@@ -1,28 +1,21 @@
 #' Find reminders on how to use a specific R function
 #'
 #' \code{reminder} scans all directories and subdirectories of a given path for files (.R, .Rmd, .txt) containing
-#'     a specific R function and prints the corresponding code.
+#'     a specific R function and prints the corresponding lines of code.
 #'
 #' @param func a function to search for.
 #' @param path a character vector, path to be scanned. The default corresponds to the working directory, getwd().
-#' @param before integer. The number of lines before the match to print.
-#' @param after integer. The number of lines after the match to print.
-#' @param rm.blank a logical value. If \code{TRUE}, all blank code lines are removed before printing.
+#' @param before integer. The number of lines to print before the function.
+#' @param after integer. The number of lines to print after the function.
 #' @param stepwise a logical value. If \code{TRUE}, examples are shown one by one using prompt.
 #' @examples
 #'# Find reminder on how to use the function chordDiagram
-#'reminder(func = "chordDiagram", path = system.file(package = "findR"), after = 7, stepwise = FALSE)
+#'reminder(func = "chordDiagram", path = system.file(package = "findR"), stepwise = FALSE)
 #' @export
 
-reminder <- function(func = "library",
-                     path = ".",
-                     before = 0,
-                     after = 0,
-                     rm.blank = FALSE,
-                     stepwise = TRUE) {
+reminder <- function(func = "library", path = ".", before = 0, after = 0, stepwise = TRUE) {
 
-# Version with prompt
-if (stepwise == TRUE) {
+  if (stepwise == TRUE) {
 
     # Get all R scripts in subdirectories
     fls <- list.files(path, pattern = "\\.r$|\\.rmd$|\\.txt$", full.names = T, recursive = T, ignore.case = T)
@@ -42,29 +35,18 @@ if (stepwise == TRUE) {
         if (x == "n") break()
         if (x == "y") x <- ""
 
-        # Exclude comments
+        # Load file
         a <- readLines(fls[i], warn = F)
-        b <- gsub(" ", "", a)
-        b <- substr(b, 0, 1)
-        a <- a[!b == "#"]
-
-        # Remove blank code lines
-        if (rm.blank == TRUE) {
-
-          b <- gsub(" ", "", a)
-          a <- a[!b == ""]
-
-        }
 
         # Show examples file by file
-        if (length(grep(paste0(func, "\\("), a)) > 0) {
+        if (sum(stringr::str_count(a, paste0("(?<![A-z\\._0-9])", func, "\\("))) > 0) {
 
           # Control input (level 2)
           if (x == "n") break()
           if (x == "y") x <- ""
 
-          # Get matching lines
-          line <- which(grepl(paste0(func, "\\("), a))
+          # Detect lines with matches
+          line <- stringr::str_which(a, paste0("(?<![A-z\\._0-9])", func, "\\("))
 
           # Update counter
           n <- n + length(line)
@@ -76,35 +58,113 @@ if (stepwise == TRUE) {
             if (x == "n") break()
             if (x == "y") x <- ""
 
-            # Define code section
-            start <- line[j] - before
-            stop <- line[j] + after
+            # Locate function in line
+            loc <- stringr::str_locate_all(a[line[j]], paste0("(?<![A-z\\._0-9])", func, "\\("))
 
-            # Limitations
-            if (start < 1) start <- 1
-            if (stop > length(a)) stop <- length(a)
+            # Detect exact position of func start and end
+            for (k in 1:(length(unlist(loc))/2)) {
 
-            # Print reminder
-            c <- NULL
+              # Control prompt (level 4)
+              if (x == "n") break()
+              if (x == "y") x <- ""
 
-            for (k in start:stop) {
+              # Trim file and replace all non-bracket patterns with 0
+              b <- stringr::str_replace_all(a[line[j]:length(a)], "[^\\(\\)]", "0")
 
-              c <- c(c, a[k])
+              # Trim first line
+              b[1] <- stringr::str_sub(b[1], unlist(loc)[k], nchar(b[1]))
 
-            }
+              # Replace brackets with +1 / -1
+              b <- stringr::str_replace_all(b, "0", " 0")
+              b <- stringr::str_replace_all(b, "\\(", " +1")
+              b <- stringr::str_replace_all(b, "\\)", " -1")
+              b <- stringr::str_trim(b, side = "left")
+              b[b == ""] <- 0
+              b <- stringr::str_split(b, " ")
 
-            cat(c, sep = "\n")
+              # Initialize line counter and bracket sum
+              lc <- 0
+              bs <- 0
 
-            # Prompt
-            while (!x %in% c("y", "n")) x <- readline("Show next example? (y/n): ")
+              # Scan trough lines until funtion is closed
+              while (lc < length(b)) {
 
+                # Line counter
+                lc <- lc + 1
+
+                # Initialize position counter
+                pc <- 0
+
+                # Sum through line
+                while (pc < length(b[[lc]])) {
+
+                  pc <- pc + 1
+
+                  bs_p <- bs
+                  bs <- bs + as.numeric(unlist(stringr::str_split(b[[lc]], " ")))[pc]
+
+                  if (bs_p == 1 & bs == 0) break()
+
+                }
+
+                if (bs_p == 1 & bs == 0) break()
+
+              }
+
+              # Line range of function
+              start_line <- line[j]
+              end_line <- line[j] + lc - 1
+
+              # Exact position of function
+              start_location <- unlist(loc)[k]
+              end_location <- pc
+
+              # Costumizable copy of a
+              a2 <- a
+
+              # Trim lines
+              if (start_line == end_line) {
+
+                a2[start_line] <- stringr::str_sub(a[start_line], start_location, start_location + end_location - 1)
+
+              } else {
+
+                a2[start_line] <- stringr::str_sub(a[start_line], start_location, nchar(a[start_line]))
+                a2[end_line] <- stringr::str_sub(a[end_line], 1, end_location)
+
+              }
+
+              # Integration of before/after
+              start_line <- start_line - before
+              end_line <- end_line + after
+
+              # Limitations
+              if (start_line < 1) start_line <- 1
+              if (end_line > length(a2)) end_line <- length(a2)
+
+              # Print reminder
+              c <- NULL
+
+              for (l in start_line:end_line) {
+
+              c <- c(c, a2[l])
+
+              }
+
+              cat(c, sep = "\n")
+
+              # Prompt
+              while (!x %in% c("y", "n")) x <- readline("Show next example? (y/n): ")
           }
 
         }
 
       }
 
-      if (n == 0) message("No examples found!")
+      }
+
+      if (n == 0) message("No examples found.")
+      if (n > 0 & !x == "n") message("No additional examples found.")
 
       } else {
 
@@ -112,79 +172,139 @@ if (stepwise == TRUE) {
 
       }
 
-    # Version wihout prompt
   } else {
 
-  # Get all R scripts in subdirectories
-  fls <- list.files(path, pattern = "\\.r$|\\.rmd$|\\.txt$", full.names = T, recursive = T, ignore.case = T)
+    # Get all R scripts in subdirectories
+    fls <- list.files(path, pattern = "\\.r$|\\.rmd$|\\.txt$", full.names = T, recursive = T, ignore.case = T)
 
-  if (length(fls) > 0) {
+    if (length(fls) > 0) {
 
-    # Counter
-    n <- 0
+      # Counter
+      n <- 0
 
-    # Scan R scripts for pattern
-    for (i in 1:length(fls)) {
+      # Scan R scripts for pattern
+      for (i in 1:length(fls)) {
 
-      # Exclude comments
-      a <- readLines(fls[i], warn = F)
-      b <- gsub(" ", "", a)
-      b <- substr(b, 0, 1)
-      a <- a[!b == "#"]
+        # Load file
+        a <- readLines(fls[i], warn = F)
 
-      # Remove blank code lines
-      if (rm.blank == TRUE) {
+        # Show examples file by file
+        if (sum(stringr::str_count(a, paste0("(?<![A-z\\._0-9])", func, "\\("))) > 0) {
 
-        b <- gsub(" ", "", a)
-        a <- a[!b == ""]
+          # Detect lines with matches
+          line <- stringr::str_which(a, paste0("(?<![A-z\\._0-9])", func, "\\("))
 
-      }
+          # Update counter
+          n <- n + length(line)
 
-      # Show examples file by file
-      if (length(grep(paste0(func, "\\("), a)) > 0) {
+          # Show one example after the other
+          for (j in 1:length(line)) {
 
-        # Get matching lines
-        line <- which(grepl(paste0(func, "\\("), a))
+            # Locate function in line
+            loc <- stringr::str_locate_all(a[line[j]], paste0("(?<![A-z\\._0-9])", func, "\\("))
 
-        # Update counter
-        n <- n + length(line)
+            # Detect exact position of func start and end
+            for (k in 1:(length(unlist(loc))/2)) {
 
-        # Show one example after the other
-        for (j in 1:length(line)) {
+              # Trim file and replace all non-bracket patterns with 0
+              b <- stringr::str_replace_all(a[line[j]:length(a)], "[^\\(\\)]", "0")
 
-          # Define code section
-          start <- line[j] - before
-          stop <- line[j] + after
+              # Trim first line
+              b[1] <- stringr::str_sub(b[1], unlist(loc)[k], nchar(b[1]))
 
-          # Limitations
-          if (start < 1) start <- 1
-          if (stop > length(a)) stop <- length(a)
+              # Replace brackets with +1 / -1
+              b <- stringr::str_replace_all(b, "0", " 0")
+              b <- stringr::str_replace_all(b, "\\(", " +1")
+              b <- stringr::str_replace_all(b, "\\)", " -1")
+              b <- stringr::str_trim(b, side = "left")
+              b[b == ""] <- 0
+              b <- stringr::str_split(b, " ")
 
-          # Print reminder
-          c <- NULL
+              # Initialize line counter and bracket sum
+              lc <- 0
+              bs <- 0
 
-          for (k in start:stop) {
+              # Scan trough lines until funtion is closed
+              while (lc < length(b)) {
 
-            c <- paste(c, a[k], sep = "\n")
+                # Line counter
+                lc <- lc + 1
+
+                # Initialize position counter
+                pc <- 0
+
+                # Sum through line
+                while (pc < length(b[[lc]])) {
+
+                  pc <- pc + 1
+
+                  bs_p <- bs
+                  bs <- bs + as.numeric(unlist(stringr::str_split(b[[lc]], " ")))[pc]
+
+                  if (bs_p == 1 & bs == 0) break()
+
+                }
+
+                if (bs_p == 1 & bs == 0) break()
+
+              }
+
+              # Line range of function
+              start_line <- line[j]
+              end_line <- line[j] + lc - 1
+
+              # Exact position of function
+              start_location <- unlist(loc)[k]
+              end_location <- pc
+
+              # Costumizable copy of a
+              a2 <- a
+
+              # Trim lines
+              if (start_line == end_line) {
+
+                a2[start_line] <- stringr::str_sub(a[start_line], start_location, start_location + end_location - 1)
+
+              } else {
+
+                a2[start_line] <- stringr::str_sub(a[start_line], start_location, nchar(a[start_line]))
+                a2[end_line] <- stringr::str_sub(a[end_line], 1, end_location)
+
+              }
+
+              # Integration of before/after
+              start_line <- start_line - before
+              end_line <- end_line + after
+
+              # Limitations
+              if (start_line < 1) start_line <- 1
+              if (end_line > length(a2)) end_line <- length(a2)
+
+              # Print reminder
+              c <- NULL
+
+              for (l in start_line:end_line) {
+
+                c <- c(c, a2[l])
+
+              }
+
+              cat(c, sep = "\n")
+
+            }
 
           }
-
-          cat(c, sep = "\n")
 
         }
 
       }
 
+      if (n == 0) message("No examples found.")
+
+    } else {
+
+      message("No files (.R, .Rmd, .txt) found!")
+
     }
-
-    if (n == 0) message("No examples found!")
-
-  } else {
-
-    message("No files (.R, .Rmd, .txt) found!")
-
   }
-
-}
-
 }
